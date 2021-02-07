915c8dcc3a6b0c3864a262650a1532fb66a3c0bf
clown <785613198@qq.com>
2021-02-07 14:53:54 +0800

    新增商品-保存SKU,SPU基本信息

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/MemberPrice.java
@@ -0,0 +1,20 @@
+package com.atguigu.gulimall.product.vo;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @program: gulimall
+ * @author: onion
+ * @create: 2021-02-06 16:31
+ **/
+
+@Data
+public class MemberPrice {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
+
+    
